d897ab64d58f3eeec380c418cdf7a97b3d5179f9
murariram2905 <murariram2905@gmail.com>
2025-05-10 22:58:49 +0530

    Add files via upload
    ageCalculator_JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+//script 
+function calculateAge() {
+    const dob = new Date (document.getElementById('dob').value);
+    const today = new Date();
+
+if (isNaN(dob.getTime())) {
+    document.getElementById('output').innerHTML="please enter a valid date.";
+    return;
+}
+let age = today.getFullYear() - dob.getFullYear();
+const monthDifference = today.getMonth() - dob.getMonth();
+
+if (monthDifference < 0 || (monthDifference === 0 &&  today.getDate() < dob.getDate())) {
+    age--;
+}
+document.getElementById('output').innerHTML = "You are " + age + " years Old.";
+}
